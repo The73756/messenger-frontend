0dcb67d2408f7e404b0f9b8ef0203441baf5b02a
The73756 <the73756the@gmail.com>
2023-05-05 21:25:22 +0900

    слияние веток

--- src/shared/ui/portal/portal.tsx
+++ src/shared/ui/portal/portal.tsx
@@ -1,10 +1,11 @@
+"use client";
+
 import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
 export const Portal: FC<PropsWithChildren> = ({ children }) => {
   const ref = useRef<Element | null>(null);
   const [mounted, setMounted] = useState(false);
-  console.log("hello");
 
   useEffect(() => {
     ref.current = document.body;
