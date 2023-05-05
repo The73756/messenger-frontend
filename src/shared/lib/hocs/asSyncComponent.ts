ca55210ae052edd379fd6b1f460a2042bae08180
The73756 <the73756the@gmail.com>
2023-05-05 21:38:48 +0900

    обновил asSyncComponent HOC

--- src/shared/lib/hocs/asSyncComponent.ts
+++ src/shared/lib/hocs/asSyncComponent.ts
@@ -1,5 +1,11 @@
 import { FC } from "react";
 
+/*
+ * This HOC created for resolve "'SomeComponent' cannot be used as a JSX component" error
+ * Issue - https://github.com/vercel/next.js/issues/42292
+ * Reference - https://github.com/vercel/next.js/issues/42292#issuecomment-1491127192
+ * */
+
 type AFC<P = {}> = (...args: Parameters<FC<P>>) => Promise<ReturnType<FC<P>>>;
 
 export const asSyncComponent = <T extends AFC<any>>(
